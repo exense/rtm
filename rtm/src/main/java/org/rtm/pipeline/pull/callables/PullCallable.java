dd4fe200b6ee93ee9b76889500e87a510c30019a
dcransac <dcransac@gmail.com>
2017-03-05 20:14:59 +0100

    Concurrency bug left in sharing acc

--- rtm/src/main/java/org/rtm/pipeline/pull/callables/PullCallable.java
+++ rtm/src/main/java/org/rtm/pipeline/pull/callables/PullCallable.java
@@ -12,22 +12,23 @@ import org.slf4j.LoggerFactory;
 public class PullCallable implements Runnable{
 
 	private static final Logger logger = LoggerFactory.getLogger(PullCallable.class);
-	
+
 	protected OptimisticRangePartitioner partitioner;
 	protected ResultHandler rh;
 	protected PullTaskBuilder tb;
-	
+
 	public PullCallable(OptimisticRangePartitioner partitioner, ResultHandler rh, PullTaskBuilder tb){
 		this.partitioner = partitioner;
 		this.rh = rh;
 		this.tb = tb;
 	}
-	
+
 	@Override
 	public final void run() {
 		try {
 			RangeBucket bucket;
 			while((bucket = splitRange()) != null){
+				logger.debug(Thread.currentThread() + " -> " + bucket);
 				harvestRange(processRange(bucket));
 			}
 		} catch (Exception e) {
@@ -38,10 +39,11 @@ public class PullCallable implements Runnable{
 	public RangeBucket splitRange(){
 		return this.partitioner.next();
 	}
-	
+
 	public LongRangeValue processRange(RangeBucket bucket) throws Exception{
 		if(bucket != null){ // due to optimistic hasNext
 			try {
+				logger.debug("Processing bucket " + bucket);
 				return tb.build().perform(bucket);
 			} catch (Exception e) {
 				logger.debug("Execution failed for bucket: " + bucket, e);
@@ -50,7 +52,7 @@ public class PullCallable implements Runnable{
 		}
 		return null;
 	}
-	
+
 	public void harvestRange(LongRangeValue lrv){
 		this.rh.attachResult(lrv);
 	}
