90547118fbd501edb4852c1cbc908a02eb3fd58f
dcransac <dcransac@gmail.com>
2017-04-05 14:46:56 +0200

    Implementing jersey API for aggregation

--- rtm/src/test/java/org/rtm/requests/RequestHandlerTest.java
+++ rtm/src/test/java/org/rtm/requests/RequestHandlerTest.java
@@ -1,5 +1,6 @@
 package org.rtm.requests;
 
+import java.io.IOException;
 import java.math.BigInteger;
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
@@ -22,6 +23,7 @@ import org.rtm.utils.DateUtils;
 import org.rtm.utils.JSONMapper;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class RequestHandlerTest {
 
@@ -33,6 +35,12 @@ public class RequestHandlerTest {
 		LocalDateTime twoWeeksAgo = today.minus(5, ChronoUnit.WEEKS);
 		LongTimeInterval lti = new LongTimeInterval(DateUtils.asDate(twoWeeksAgo).getTime(), DateUtils.asDate(today).getTime());
 		AggregationRequest ar = new AggregationRequest(lti, TestSelectorBuilder.buildSimpleSelectorList(), new Properties());
+		ObjectMapper om = new ObjectMapper();
+		try {
+			om.writeValue(System.out, ar);
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
 		
 		Integer targetDots = 10;
 		ar.getProperties().put("targetChartDots", targetDots.toString());
