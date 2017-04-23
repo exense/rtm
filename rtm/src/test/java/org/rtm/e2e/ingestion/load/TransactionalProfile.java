66bacbecfc68a30cc33a620c570bc12008ca9924
dcransac <dcransac@gmail.com>
2017-04-23 12:23:48 +0200

    Decouple Tasks & Transactions (modulo) + Mongo thread queuing handling

--- rtm/src/test/java/org/rtm/e2e/ingestion/load/TransactionalProfile.java
+++ rtm/src/test/java/org/rtm/e2e/ingestion/load/TransactionalProfile.java
@@ -11,27 +11,36 @@ public class TransactionalProfile extends LoadDescriptor{
 
 	private int skewFactor;
 	private int stdFactor;
+	private int targetCardinality;
 
-	public TransactionalProfile(long pauseTime, int nbIterations, int nbTasks, int timeOut, int skewFactor, int stdFactor){
+	public TransactionalProfile(long pauseTime, int nbIterations, int nbTasks, int timeOut, int skewFactor, int stdFactor, int targetCardinality){
 		super(pauseTime, nbIterations, nbTasks, timeOut);
 		this.stdFactor = stdFactor;
 		this.skewFactor = skewFactor;
+		this.targetCardinality = targetCardinality;
 	}
 
 	@Override
 	public Map<String, Object> getNextMeasurementForSend(int taskId) {
+		
+		int actualTaskId = (taskId % this.targetCardinality) + 1;
+		
 		return TestMeasurementBuilder.build(TestMeasurementType.SIMPLE,
 				"TransactionalProfile",
-				"Transaction_" + taskId,
+				"Transaction_" + actualTaskId,
 				new Date().getTime(),
 				computeSkew(taskId),
 				null);
 	}
 
 	private Long computeSkew(int taskId) {
-		long smallDeviation= ThreadLocalRandom.current().nextLong(0, taskId * stdFactor);
+		
+		int actualTaskId = (taskId % this.targetCardinality) + 1;
+		
+		long targetDeviation = actualTaskId * stdFactor;
+		long smallDeviation= ThreadLocalRandom.current().nextLong(0, targetDeviation > 0 ? targetDeviation : 1L);
 		boolean plusMinus = ThreadLocalRandom.current().nextInt(0,2) == 1;
-		Long value = new Long(taskId * this.skewFactor);
+		Long value = new Long(actualTaskId * this.skewFactor);
 		if(plusMinus)
 			value += smallDeviation;
 		else
