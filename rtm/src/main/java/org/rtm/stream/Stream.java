5be88e9bf23c7deb5be3f5474222f873bdd1988b
dcransac <dcransac@gmail.com>
2017-04-23 12:16:29 +0200

    Implemented Stream lifecycle mgmt & eviction

--- rtm/src/main/java/org/rtm/stream/Stream.java
+++ rtm/src/main/java/org/rtm/stream/Stream.java
@@ -2,6 +2,8 @@ package org.rtm.stream;
 
 import java.util.concurrent.ConcurrentSkipListMap;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 /*
  * Wraps a CSLM which contains an ordered representation of the entire timeseries data of the stream 
  */
@@ -9,7 +11,37 @@ public class Stream<T> extends ConcurrentSkipListMap<Long, AggregationResult<T>>
 
 	private static final long serialVersionUID = 8431902330094695624L;
 	
+	@JsonIgnore
+	private long timeCreated;
+	
+	private boolean isComplete = false;
+	
+	@JsonIgnore
+	private boolean isRefreshedSinceCompletion = false;
+	
 	public Stream(){
 		super();
+		this.timeCreated = System.currentTimeMillis();
 	}
+
+	public long getTimeCreated() {
+		return timeCreated;
+	}
+
+	public synchronized boolean isComplete() {
+		return isComplete;
+	}
+
+	public synchronized void setComplete(boolean isComplete) {
+		this.isComplete = isComplete;
+	}
+
+	public synchronized boolean isRefreshedSinceCompletion() {
+		return isRefreshedSinceCompletion;
+	}
+
+	public synchronized void setRefreshedSinceCompletion(boolean isRefreshedSinceCompletion) {
+		this.isRefreshedSinceCompletion = isRefreshedSinceCompletion;
+	}
+
 }
