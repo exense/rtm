e0d0dd17de4f4acb7978dd0982afcb720ebed71d
Dorian Cransac <reachme@doriancransac.info>
2019-09-15 16:55:46 +0200

    Adding dashboard mapping object

--- rtm/src/main/java/org/rtm/rest/dashboard/Dashboard.java
+++ rtm/src/main/java/org/rtm/rest/dashboard/Dashboard.java
@@ -1,5 +1,29 @@
 package org.rtm.rest.dashboard;
 
+import java.util.List;
+
 public class Dashboard {
 
+	private String title;
+	private String dashboardid;
+	private List<Object> widgets;
+	
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getDashboardid() {
+		return dashboardid;
+	}
+	public void setDashboardid(String dashboardid) {
+		this.dashboardid = dashboardid;
+	}
+	public List<Object> getWidgets() {
+		return widgets;
+	}
+	public void setWidgets(List<Object> widgets) {
+		this.widgets = widgets;
+	}
 }
