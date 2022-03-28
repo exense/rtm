4da91b57f67b45d9042dab47146fa6b0f9c1efa1
David Stephan <David.Stephan@exense.ch>
2022-03-28 13:29:11 +0200

    Revert "Fixing junit after percentile refactoring"
    This reverts commit fe5ac5635704361b5fc8278e1f932a5f555aeabe.

--- rtm/src/test/java/org/rtm/requests/RequestHandlerTest.java
+++ rtm/src/test/java/org/rtm/requests/RequestHandlerTest.java
@@ -10,10 +10,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.IntStream;
 
-import ch.exense.commons.app.Configuration;
-import step.core.collections.mongodb.MongoDBCollectionFactory;
-import step.core.collections.Document;
-import org.rtm.commons.MeasurementAccessor;
+import org.junit.Test;
 import org.rtm.metrics.postprocessing.MetricsManager;
 import org.rtm.range.time.LongTimeInterval;
 import org.rtm.request.AbstractResponse;
@@ -31,7 +28,6 @@ import org.rtm.utils.DateUtils;
 import org.rtm.utils.JSONMapper;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import step.core.collections.mongodb.MongoDBCollectionFactory;
 
 //TODO: mongo mock
 public class RequestHandlerTest {
@@ -88,9 +84,6 @@ public class RequestHandlerTest {
 	public void basicTest(int histApp, int histSize, int partition, List<Long> timeToFirstByte, List<Long> elapse, List<Long> elapseWithResults) throws Exception{
 		Properties props = new Properties();
 		props.load(new FileReader(new File("src/main/resources/rtm.properties")));
-
-		Configuration configuration = new Configuration(new File("src/main/resources/rtm.properties"));
-		MeasurementAccessor ma = new MeasurementAccessor(new MongoDBCollectionFactory(configuration.getUnderlyingPropertyObject()).getCollection(MeasurementAccessor.ENTITY_NAME, Document.class));
 		
 		LocalDateTime today = LocalDateTime.now();
 		LocalDateTime twoWeeksAgo = today.minus(10, ChronoUnit.WEEKS);
@@ -115,8 +108,8 @@ public class RequestHandlerTest {
 
 		//ar.getServiceParams().put("targetChartDots", "1");
 
-		StreamBroker ssm = new StreamBroker(configuration);
-		RequestHandler rh = new RequestHandler(ssm, configuration, ma);
+		StreamBroker ssm = new StreamBroker();
+		RequestHandler rh = new RequestHandler(ssm);
 
 		IntStream.rangeClosed(1, 1).forEach(it -> {
 
@@ -166,7 +159,7 @@ public class RequestHandlerTest {
 			String[] metricKeys = {"cnt","avg","min","max","50th pcl","tps","tpm","80th pcl","90th pcl","99th pcl"};
 
 			try {
-				result = new MetricsManager(fknProps, configuration).handle(stream);
+				result = new MetricsManager(fknProps).handle(stream);
 				FinalAggregationResult f = (FinalAggregationResult) result.getStreamData().firstEntry().getValue();
 				System.out.println("cnt,avg,min,max,50th pcl,tps,tpm,80th pcl,90th pcl,99th pcl");
 				result.getStreamData().forEach((k,v) -> {
